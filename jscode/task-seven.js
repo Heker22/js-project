2c56352dc1f202a59a22406dc6d8d51eb6de618d
Heker22 <146105661+Heker22@users.noreply.github.com>
2025-10-23 15:14:02 +0200

    d

---
+++ jscode/task-seven.js
@@ -0,0 +1,33 @@
+
+const field = document.querySelector('.football__box');
+
+const ball = document.querySelector('.football__img');
+
+field.addEventListener('click', event => {
+
+    const fieldWidth = field.offsetWidth;
+
+    const fieldHeight = field.offsetHeight;
+
+    const ballWidth = ball.offsetWidth;
+
+    const ballHeight = ball.offsetHeight;
+
+    let clickX = event.pageX - field.offsetLeft;
+
+    let clickY = event.pageY - field.offsetTop;
+
+    const minX = ballWidth / 2;
+
+    const minY = ballHeight / 2;
+
+    const maxX = fieldWidth - ballWidth / 2;
+
+    const maxY = fieldHeight - ballHeight / 2;
+
+    const ballX = Math.min(Math.max(clickX, minX), maxX);
+
+    const ballY = Math.min(Math.max(clickY, minY), maxY);
+
+    ball.style.transform = `translate(${ballX - minX}px, ${ballY - minY}px)`;
+})
